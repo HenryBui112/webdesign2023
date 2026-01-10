5d091a66ffbf790ef333af828dabf32d8e66dc9f
HenryBui112 <149218547+HenryBui112@users.noreply.github.com>
2026-01-10 11:40:07 +0700

    Add files via upload

---
+++ RMDW/Pages/App.jsx
@@ -0,0 +1,15 @@
+import Header from "../src/component/Header";
+import Footer from "../src/component/Footer";
+import Home from "./Home";
+
+function App () {
+    return (
+        <>
+        <Header/>
+        <Footer/>
+        <Home/>
+        </>
+    );
+}
+
+export default App;
